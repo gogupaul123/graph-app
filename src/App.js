f0f3c673ecd35a40ed98ab197bafbb977cb8c808
Gogu Paul <68021113+gogupaul123@users.noreply.github.com>
2021-04-06 00:09:39 +0300

    ll

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import Content from './components/Content';
 
-function App() {
+function App() { 
+
+  const [posts, updatePosts] = useState();
+
+
+  //Fetching the data only when the page loads for the first time => useEffect  []
+  useEffect(()=>{
+    fetch('https://fakerql.stephix.uk/graphql', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({ query: `{
+    allPosts(count: 5) {
+      published
+      createdAt
+    }
+  }` }),
+})
+  .then(data => data.json())
+  .then(res => updatePosts(res));
+  }, [])
+
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Content content={posts}/>
+    </>
+  )
 }
 
 export default App;
+
---
+++ src/components/Content.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function Content({content}) {
+ 
+  console.log(content)
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Content;
